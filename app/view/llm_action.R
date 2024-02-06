5f34c5d088a196bcac28022374a0022a67fcae21
Ían Muliterno <iannimuliterno@gmail.com>
2024-02-06 14:00:29 -0300

    LLM view and related logic creation

---
+++ app/view/llm_action.R
@@ -0,0 +1,31 @@
+box::use(
+    shiny,
+)
+
+box::use(
+    app/logic/prompt_fun
+)
+
+#' @export 
+ui <- function(id) {
+    ns <- NS(id)
+    box(shiny$renderText("output_ai"))
+}
+
+
+
+
+#' @export 
+server <- function(id,your_key,position_input,desc_input,company_input,type_input,exp_input) {
+    moduleServer(
+        id = id,
+        module = function(session,input,output) {
+
+            your_prpt <- prompt_gen(position_input,desc_input,company_input,type_input,exp_input)
+            API_connection$your_llm(prompt = your_prpt,your_bard_key = your_key)
+
+        }
+    )    
+    
+
+}
