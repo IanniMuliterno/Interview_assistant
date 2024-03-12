bdb5d5cb50d3b983a29ec3da284c2b3a27173259
Ían Muliterno <iannimuliterno@gmail.com>
2024-03-12 19:18:48 -0300

    fixing id declaration in e2e test

--- tests/cypress/e2e/app.cy.js
+++ tests/cypress/e2e/app.cy.js
@@ -4,8 +4,8 @@ describe('app', () => {
   })
 
   it('Trying to start tailoring questions without sending bard key', () => {
-     cy.get('app-key-input_text').clear();
-     cy.get('app-start').click();
-    cy.get('app-out_ai-output_text').contains('bard key'); 
+     cy.get('#app-key-input_text').clear();
+     cy.get('#app-start').click();
+    cy.get('#app-out_ai-output_text').contains('bard key'); 
   })
 })
